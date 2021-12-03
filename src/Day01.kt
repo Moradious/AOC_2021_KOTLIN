0ac0c9b874904fce1ef2d9687040ecee60dcf4cb
Morad Azzouzi <morad.azzouzi@innovorder.fr>
2021-12-04 00:31:06 +0100

    Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { it[0] < it[1] }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input
+            .windowed(3) { it.sum() }
+            .windowed(2)
+            .count { it[0] < it[1] }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    println(part1(testInput))
+    println(part2(testInput))
 }
