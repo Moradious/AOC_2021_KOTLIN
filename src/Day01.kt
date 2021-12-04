9c8e4cd0a65dfe1eccb40d479595a60f7bd09856
Morad Azzouzi <morad.azzouzi@innovorder.fr>
2021-12-04 01:48:25 +0100

    Make it an extension

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,17 +2,20 @@
 fun main() {
 
     fun part1(input: List<Int>): Int {
-        return input.windowed(2).count { it[0] < it[1] }
+        return input.windowed(2).countSuperiority()
     }
 
     fun part2(input: List<Int>): Int {
         return input
             .windowed(3) { it.sum() }
             .windowed(2)
-            .count { it[0] < it[1] }
+            .countSuperiority()
     }
 
     val testInput = readInput("Day01_test").map { it.toInt() }
     println(part1(testInput))
     println(part2(testInput))
 }
+
+fun List<List<Int>>.countSuperiority(): Int = count { it[0] < it[1] }
+
