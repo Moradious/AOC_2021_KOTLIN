b749b7e85c7ffd342c755a0b27c8a0b4dcd151e6
Morad Azzouzi <morad.azzouzi@innovorder.fr>
2021-12-07 21:12:33 +0100

    Day07 part 1 & 2

---
+++ src/Day07.kt
@@ -0,0 +1,24 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+
+    fun part1(input: List<Int>): Int? =
+        input
+            .map { number -> input.map { entry -> max(number, entry) - min(number, entry) }.sum() }
+            .minOrNull()
+
+    fun part2(input: List<Int>): Int? {
+        return input
+            .map { number -> input.map { entry -> getTriangularNumber((max(number, entry) - min(number, entry))) }.sum() }
+            .minOrNull()
+    }
+
+    val testInput = readInput("Day07_data")
+    val data = testInput[0].split(",").map { it.toInt() }
+    println(part1(data))
+    println(part2(data))
+}
+
+fun getTriangularNumber(moves: Int): Int = moves * (moves + 1) / 2
+
