642a077c4ebf7be7afe692919632844e7091bf9b
Morad Azzouzi <morad.azzouzi@innovorder.fr>
2021-12-07 15:48:52 +0100

    Day03 part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -19,11 +19,51 @@ fun main() {
         val gammaRateInteger = Integer.parseInt(gammaRate, 2)
         val epsilonRateInteger = Integer.parseInt(epsilonRate, 2)
 
-        println("Result : " + gammaRateInteger * epsilonRateInteger)
+        println("Result part1 : " + gammaRateInteger * epsilonRateInteger)
+    }
+
+    fun part2(input: List<String>) {
+
+        var oxygenRating = input.map { it }
+        var scruberRating = input.map { it }
+
+        loop@ for (index in oxygenRating[0].indices) {
+            oxygenRating
+                .map { it[index] }
+                .partition { it == '0' }
+                .apply {
+                    oxygenRating = if (first.size > second.size) {
+                        oxygenRating.filter { it[index] == '0' }
+                    } else {
+                        oxygenRating.filter { it[index] == '1' }
+                    }
+                }
+
+            if (oxygenRating.size == 1) break@loop
+        }
+
+        loop@ for (index in scruberRating[0].indices) {
+                scruberRating
+                    .map { it[index] }
+                    .partition { it == '0' }
+                    .apply {
+                        scruberRating = if (first.size > second.size) {
+                            scruberRating.filter { it[index] == '1' }
+                        } else {
+                            scruberRating.filter { it[index] == '0' }
+                        }
+                    }
+
+            if (scruberRating.size == 1) break@loop
+        }
+
+        val result = Integer.parseInt(oxygenRating[0], 2) * Integer.parseInt(scruberRating[0], 2)
+        println("Result : $result")
     }
 
     val testInput = readInput("Day03_data")
     part1(testInput)
+    part2(testInput)
 }
 
 
